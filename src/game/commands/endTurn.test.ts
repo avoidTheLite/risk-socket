d57b2aba5cf63fafce60a27cea70063313919556
avoidTheLite <metalsupremacist@gmail.com>
2025-03-03 13:34:07 -0500

    finished endTurn tests

--- src/game/commands/endTurn.test.ts
+++ src/game/commands/endTurn.test.ts
@@ -10,12 +10,12 @@ describe('endTurn', () => {
     // then the game phase should change to 'play' and turn should be set to 1
     test('should end deploy phase and start play phase with turn = 1', async () => {
         let game: Game = createTestGame(4);
+        // armies have not been distributed
         game = await endTurn(game);
         expect(game.phase).toBe('play');
         expect(game.turn).toBe(1);
 
     })
-
 
     // given that game phase = 'deploy' and at least one player has armies to deploy
     // when endTurn is called
@@ -36,9 +36,27 @@ describe('endTurn', () => {
     // when endTurn is called
     // then turn should be set to 2
     // and actvePlayerIndex should be set to 1
-
+    test(' should stay in play phase and increase turn', async () => {
+        let game: Game = createTestGame(4);
+        game.phase = 'play';
+        game = await endTurn(game);
+        expect(game.phase).toBe('play');
+        expect(game.turn).toBe(2);
+        expect(game.activePlayerIndex).toBe(1);
+    })
     //given that game phase == 'play' and turn = 4 = number of players
     // when endTurn is called
     // then turn should be set to 5
     // and actvePlayerIndex should be set to 0
+
+    test('When last player ends turn, game should stay in play phase and go back to first player', async () => {
+        let game: Game = createTestGame(4);
+        game.phase = 'play';
+        game.activePlayerIndex = 3;
+        game.turn = 4;
+        game = await endTurn(game);
+        expect(game.phase).toBe('play');
+        expect(game.turn).toBe(5);
+        expect(game.activePlayerIndex).toBe(0);
+    })
 })
