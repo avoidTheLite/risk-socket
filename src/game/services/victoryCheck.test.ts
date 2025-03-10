66b1804ec58828d5eb2478cf145f07f6a37ca62a
Justin Laptop <metalsupremacist@gmail.com>
2025-03-10 18:54:40 -0400

    added victoryCheck test to exclude neutral armies

--- src/game/services/victoryCheck.test.ts
+++ src/game/services/victoryCheck.test.ts
@@ -19,7 +19,7 @@ describe('Victory Check', () => {
 
     test('should return false if game is not won', () => {
         let game: Game = createTestGame(4);
-        for (let i = 0; i < game.countries.length; i++) {
+        for (let i = 0; i < game.countries.length - 1; i++) {
             game.countries[i].ownerID = 0;
             game.countries[i].armies = 1;
         }
@@ -27,5 +27,18 @@ describe('Victory Check', () => {
         game.countries[game.countries.length - 1].armies = 1;
 
         expect(victoryCheck(game)).toBe(false)
+    })
+
+    test('should return true even if neutral armies remain', () => {
+        let game: Game = createTestGame(2);
+        for (let i = 0; i < game.countries.length - 2; i++) {
+            game.countries[i].ownerID = 0;
+            game.countries[i].armies = 1;
+        }
+        game.countries[game.countries.length - 2].ownerID = 99;
+        game.countries[game.countries.length - 2].armies = 1;
+        game.countries[game.countries.length - 1].ownerID = 1;
+        game.countries[game.countries.length - 1].armies = 0;
+        expect(victoryCheck(game)).toBe(true)
     })
 })
