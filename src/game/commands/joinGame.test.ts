602ac9a8258f796b812d743f99553e20a4970727
Justin Laptop <metalsupremacist@gmail.com>
2025-04-17 18:36:47 -0400

    return gameState when joining game

--- src/game/commands/joinGame.test.ts
+++ src/game/commands/joinGame.test.ts
@@ -12,7 +12,7 @@ import joinGame from "./joinGame";
 import { manager } from "../../common/util/createWebSocketServer";
 import { joinGameError } from "../../common/types/errors";
 import { WebSocket } from "ws";
-import { WsResponse } from "../../common/types/types";
+import { WsResponse, Game } from "../../common/types/types";
 import createMockSocket from "../../common/util/test/createMockSocket";
 import createTestGame from "../../common/util/test/createTestGame";
 
@@ -25,26 +25,27 @@ describe('Join Game - Unit Tests', () => {
 
     test('Successfully joins an open game', () => {
         const ws = createMockSocket();
-        const testSaveName = 'test save name 1';
+        const game = createTestGame(4);
         const playerSlots = [2, 3];
-        const response = joinGame(ws, testSaveName, playerSlots);
+        const response = joinGame(ws, game, playerSlots);
         expect(response.data.message).toBe(`Successfully joined Player Slots: ${playerSlots.join(', ')}`); 
         expect(response.data.status).toBe('success');
-
+        expect(response.data.gameState).toBeInstanceOf(Game);
+        
     })
-
+    
     test('throws an error if there are no open game slots', () => {
         const ws = createMockSocket();
-        const testSaveName = 'test save name 1';
+        const game = createTestGame(4);
         const playerSlots = [2, 3];
-        expect(() => joinGame(ws, testSaveName, playerSlots)).toThrow(joinGameError);
+        expect(() => joinGame(ws, game, playerSlots)).toThrow(joinGameError);
     })
-
+    
     test('throws an error if the player passes invalid player IDs', () => {
         const ws = createMockSocket();
-        const testSaveName = 'test save name 1';
+        const game = createTestGame(4);
         const playerSlots = [2, 3, 4];
-        expect(() => joinGame(ws, testSaveName, playerSlots)).toThrow(joinGameError);
+        expect(() => joinGame(ws, game, playerSlots)).toThrow(joinGameError);
     })
 
 })
--- src/game/commands/joinGame.ts
+++ src/game/commands/joinGame.ts
@@ -1,13 +1,13 @@
 import { manager } from "../../common/util/createWebSocketServer";
-import { WsResponse } from "../../common/types/types";
+import { WsResponse, Game } from "../../common/types/types";
 import { joinGameError } from "../../common/types/errors";
 import { WebSocket } from "ws";
 
-export default function joinGame(ws: WebSocket, saveName: string, playerSlots: number[]): WsResponse{
+export default function joinGame(ws: WebSocket, game: Game, playerSlots: number[]): WsResponse{
 
-    const availableGameSlots: number[] = manager.getOpenGameSlots(saveName);
+    const availableGameSlots: number[] = manager.getOpenGameSlots(game.saveName);
     if(availableGameSlots.length === 0) {
-        throw new joinGameError({ message: `Unable to join game. There are no open game slots for ${saveName}` });
+        throw new joinGameError({ message: `Unable to join game. There are no open game slots for ${game.saveName}` });
     }
 
     let invalidPlayerIDs: number[] = playerSlots.filter((id) => !availableGameSlots.includes(id));
@@ -16,13 +16,14 @@ export default function joinGame(ws: WebSocket, saveName: string, playerSlots: n
     }
 
     let playerSlotsToJoin: number[] = playerSlots.filter((id) => availableGameSlots.includes(id));
-    manager.connectToGame(ws, saveName, playerSlotsToJoin);
+    manager.connectToGame(ws, game.saveName, playerSlotsToJoin);
 
     const response: WsResponse = {
         data: {
             action: 'joinGame',
             message: `Successfully joined Player Slots: ${playerSlotsToJoin.join(', ')}`,
             status: 'success',
+            gameState: game
         }
     }
     return response
